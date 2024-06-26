ff6c5ad6eedbb8910448bc5176b558122892f660
AlbinaShaiba <shaibakova@ptiufa.ru>
2024-06-26 12:26:51 +0500

    .

--- homework.py
+++ homework.py
@@ -33,15 +33,9 @@ HOMEWORK_VERDICTS = {
 logging.basicConfig(
     level=logging.DEBUG,
     filename='main.log',
+    encoding='utf-8',
     filemode='w'
 )
-
-logging.debug('123')
-logging.info('Сообщение отправлено')
-logging.warning('Большая нагрузка!')
-logging.error('Бот не смог отправить сообщение')
-logging.critical('Всё упало! Зовите админа!1!111') 
-
 
 
 class VarIsNotAvailable(Exception):
